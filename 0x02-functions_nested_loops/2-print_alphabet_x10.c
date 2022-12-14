f0b002b2d8a29f2c2228457e7599125690c73637
MatikoMarwa <matikomkorofi@gmail.com>
2022-12-14 13:26:44 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alpha_x10 function will print the alphabet 10 times in lower case
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+	for (i = 0; i <= 9; i++ ){
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		        _putchar('\n');
+	}
+}
