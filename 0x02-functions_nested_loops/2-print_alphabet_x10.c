5bf436b16f6ae12f909bb0532452041b629e8a51
MatikoMarwa <matikomkorofi@gmail.com>
2022-12-14 13:45:43 +0000

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char letter;
-	for (i = 0; i <= 9; i++ ){
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		        _putchar('\n');
-	}
+int i;
+char letter;
+for (i = 0; i <= 9; i++ ){
+for (letter = 'a'; letter <= 'z'; letter++){
+_putchar(letter);
+}
+_putchar('\n');
+}
 }
