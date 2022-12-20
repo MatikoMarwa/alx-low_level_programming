62aab2605600200e9f67bd665c2dba947be96f84
MatikoMarwa <matikomkorofi@gmail.com>
2022-12-20 17:30:10 +0000

    All at once

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", numi);
+return (0);
+}
